0e604379300dea326bbbeba2fdbb04ff1a92432d
chanwukim <chnwu.kim@gmail.com>
2024-10-07 17:41:09 +0900

    터보레포 #1

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,65 +4,21 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  parser: "@typescript-eslint/parser",
   settings: {
     "import/resolver": {
       typescript: {
         project,
       },
     },
-    react: {
-      version: "detect",
-    },
   },
-  globals: {
-    React: true, // React를 import 없이 사용 가능
-    JSX: true, // JSX 문법 사용 가능
-  },
-  env: {
-    node: true, // Node.js의 전역 객체와 모듈 사용 가능 (process, require 등)
-    browser: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "prettier",
-    "turbo",
-    require.resolve("@vercel/style-guide/eslint/next"),
-  ],
-  plugins: ["@typescript-eslint"],
+  // https://nextjs.org/docs/app/building-your-application/configuring/eslint
+  extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+
   rules: {
-    curly: "error", // 모든 제어문 중괄호
-    "no-implicit-coercion": "error", // 암시적 타입 변환을 금지
     eqeqeq: ["error", "always", { null: "ignore" }], // 동등 비교(===, !==)를 사용. null 제외
-    "require-await": "error", // async 함수는 await 사용 필수
-    "import/no-duplicates": ["error", { considerQueryString: true }], // 중복 import 금지
-
+    "@typescript-eslint/consistent-type-imports": "error", // import type을 사용하도록 강제
+    "@typescript-eslint/no-empty-object-type": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/ban-ts-comment": "off", // @ts-<directive> 사용 가능
-    "@typescript-eslint/consistent-type-imports": "error", // 타입만 import할 때는 import type을 사용하도록 강제
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        format: ["camelCase", "UPPER_CASE", "PascalCase"],
-        selector: "variable",
-        leadingUnderscore: "allow",
-      },
-      { format: ["camelCase", "PascalCase"], selector: "function" },
-      { format: ["PascalCase"], selector: "interface" },
-      { format: ["PascalCase"], selector: "typeAlias" },
-    ],
-
-    "react/prop-types": "off", // prop-types 사용 금지
-    "react/react-in-jsx-scope": "off", // React를 import 하지 않아도 JSX 사용 가능. React 17 이상에서는 불필요
-    "react/jsx-uses-react": "off", // React 17 이상에서는 불필요
-    "react/display-name": "off", // display name 검사 비활성화 (React.memo, React.forwardRef)
-    "react/no-unknown-property": "error",
-
     "import/order": [
       "error",
       {
@@ -78,11 +34,29 @@ module.exports = {
             group: "external",
             position: "before",
           },
+          {
+            pattern: "@/features/**/*",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@/lib/**/*",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@/hooks/**/*",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@/components/**/*",
+            group: "external",
+            position: "after",
+          },
         ],
         pathGroupsExcludedImportTypes: ["react", "next"],
       },
     ],
   },
-  overrides: [{ files: ["*.ts?(x)"] }],
-  ignorePatterns: [".*.js", "node_modules/", "dist/"],
 };
